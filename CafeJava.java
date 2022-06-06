744cbdafa8cfb9f334ba9ec543ab3a7da8480952
Tiyarong Gurung <dipeshtamu95@gmail.com>
2022-06-05 22:48:32 -0400

    completed

---
+++ CafeJava.java
@@ -0,0 +1,53 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        double coffePrice = 2;
+        double lattePrice = 4.5;
+        double cappuccinoPrice = 3;
+
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
+
+        // Order Status
+        // Cindhuri's order
+        System.out.println(generalGreeting + customer1);
+        System.out.println(customer1 + pendingMessage);
+
+        // Noah's Order
+        if (isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + cappuccinoPrice);
+        } else {
+            System.out.println(customer4 + pendingMessage);
+        }
+
+        // Sam's order
+
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        } else {
+            System.out.println(customer2 + pendingMessage);
+        }
+
+        System.out.println(displayTotalMessage + 2 * lattePrice);
+
+        // Jimmy owes
+
+        System.out.println(displayTotalMessage + ( lattePrice - coffePrice ));
+    }
+}
--- Person.java
+++
@@ -1,7 +0,0 @@
-public class Person {
-    public static void main(String[] args) {
-        System.out.println("Hello my name is Tiyarong");
-        System.out.println("I am 25 years old");
-        System.out.println("And I live in Buffalo NY");
-    }
-}
